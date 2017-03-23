d947e23468cdadd55f63889361684bd70e2db389
Chris Bowdon <c.bowdon@bath.edu>
2017-03-23 22:07:54 +0000

    Add some user configuration options

--- bashistrano_conf.sh
+++ bashistrano_conf.sh
@@ -1,2 +1,9 @@
+## Host details
 user='chris'
 servers='macos localhost'
+
+## remote_PATH :: PATH to set on remote machine. Defaults to the PATH SSH normally sets.
+# remote_PATH='/usr/bin:/bin/'
+
+## SSH command to execute. Defaults to 'ssh -ttq'.
+# ssh_cmd='ssh -ttq'
